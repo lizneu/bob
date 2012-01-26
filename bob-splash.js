3a9bd72c16fc8ba8560e763cd65adebca9cd8a53
jonahkagan <jonahkagan@gmail.com>
2012-01-25 23:01:24 -0500

    deleted old code

--- bob-splash.js
+++ bob-splash.js
@@ -31,50 +31,6 @@ var BOB = (function() {
       });
     });
   };
-    /*
-  me.init = (function() {
-    function makeAutocomplete(num, field) {
-      var idBase = '#fav' + num + '-';
-      var id = idBase + field;
-      $(id).autocomplete({
-        source: _lists[field],
-        select: function(event, ui) {
-          $(idBase + 'code').val(ui.item.course.code);
-          $(idBase + 'name').val(ui.item.course.name);
-          $(idBase + 'prof').val(ui.item.course.professor);
-          return false;
-        }
-      })
-    }
-    return function() {
-      me.getAllCourses(function(response) {
-        _courses = response;
-        _lists.code = _createFieldList('code');
-        _lists.name = _createFieldList('name');
-        _lists.prof = _createFieldList('prof');
-
-        for(var i=1; i<=3; i++) {
-          makeAutocomplete(i, 'code');
-          makeAutocomplete(i, 'name');
-          makeAutocomplete(i, 'prof');
-        }
-      });
-    };
-  }());
-    */
-
-  /*
-  function _createFieldList(field) {
-    var list = [];
-    for (var i = 0; i < _courses.length; i++) {
-      list.push({
-        label: _courses[i][field],
-        course: _courses[i] 
-      });
-    }
-    return list;
-  }
-    */
 
   return me;
 }());
