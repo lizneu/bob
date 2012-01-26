74c76da4c44627f0b85cd357593eb77d6456b3e4
jonahkagan <jonahkagan@gmail.com>
2012-01-25 23:37:20 -0500

    basic dropdown working and submit button enabling

--- bob-splash.js
+++ bob-splash.js
@@ -1,7 +1,7 @@
 var BOB = (function() {
   var me = {};
 
-  var _courses, _lists = {};
+  var _courses;
 
   me.getAllCourses = function(callback) {
     console.log('get');
@@ -14,7 +14,19 @@ var BOB = (function() {
     });
   };
 
+  function _toggleSubmit() {
+    var someEmpty = false;
+    for (var i = 1; i <= 3; i++) {
+      if ($('#fav' + i).val() === '') {
+        someEmpty = true;
+        break;
+      }
+    }
+    $('#submit').button('option', 'disabled', someEmpty);
+  }
+
   me.init = function() {
+    $('#submit').button({ 'disabled' : true });
     me.getAllCourses(function(response) {
       var courses = response.map(function(course) {
         return {
@@ -23,12 +35,27 @@ var BOB = (function() {
           cid: course.cid
         };
       });
-      $('#fav1').autocomplete({
-        source: courses
-      })
-      .bind('autocompleteselect', function(event, ui) {
-        // TODO mark or remove dups
-      });
+      for (var i = 1; i <= 3; i++) {
+        $('#fav' + i).autocomplete({
+          source: courses
+        })
+        .bind('autocompleteselect', function(event, ui) {
+          _toggleSubmit();
+          // TODO mark or remove dups
+          // TODO do something with the cid
+        })
+        .change(_toggleSubmit)
+          /* TODO figure out a way to make it only catch backspaces
+                after a course has been selected
+        .keydown(function(event) {
+          if (event.keyCode === 8) { // catch backspace
+            $(this).val('');
+          }
+        });
+        /*
+
+      }
+
     });
   };
 
